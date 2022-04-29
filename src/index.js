ea8cd0f93db94a40a1085e937e057f3f866ce087
Bondi <kirkizon5@gmail.com>
2022-04-30 01:07:57 +0300

    1

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter basename={process.env.PUBLIC_URL}>
 		<Provider store={store}>
 			<App />
 
 		</Provider>
 
-	</BrowserRouter>,
+	</HashRouter>,
 
 	document.getElementById('root')
 );
